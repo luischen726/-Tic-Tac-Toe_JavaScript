1f4033211a68de3b1128c5dac241d8b33a89ebcb
luischen726 <luischen726@outlook.com>
2025-09-21 22:57:25 +0800

    [feat] load new assets for tic Tac toe project

--- pixi_Front_End/src/main.ts
+++ pixi_Front_End/src/main.ts
@@ -1,4 +1,11 @@
-import { Application, Assets, Sprite } from "pixi.js";
+import {
+  Application,
+  Assets,
+  Sprite,
+  Container,
+  Text,
+  TextStyle,
+} from "pixi.js";
 
 (async () => {
   // Create a new application
@@ -12,24 +19,60 @@ import { Application, Assets, Sprite } from "pixi.js";
 
   // Load the bunny texture
   const texture = await Assets.load("/assets/bunny.png");
+  const backgroundTexture = await Assets.load("/assets/background.jpg");
+  const boardTexture = await Assets.load("/assets/board.png");
+  const logoTexture = await Assets.load("/assets/logo.png");
 
   // Create a bunny Sprite
   const bunny = new Sprite(texture);
+  const background = new Sprite(backgroundTexture);
+  const board = new Sprite(boardTexture);
+  const logo = new Sprite(logoTexture);
+
+  const instructionText = new Text({
+    text: "Click to start",
+    style: new TextStyle({
+      fontFamily: ["Helvetica", "Arial", "sans-serif"],
+      fontSize: 36,
+      fill: 0x000,
+      align: "center",
+    }),
+  });
+  instructionText.pivot.x = instructionText.width / 2;
+  instructionText.pivot.y = instructionText.height / 2;
 
   // Center the sprite's anchor point
+
   bunny.anchor.set(0.5);
+  background.anchor.set(0.5);
+  board.anchor.set(0.5);
 
   // Move the sprite to the center of the screen
   bunny.position.set(app.screen.width / 2, app.screen.height / 2);
+  // background.position.set(app.screen.width / 2, app.screen.height / 2);
+  // board.position.set(app.screen.width / 2, app.screen.height / 2);
+  const titleScreen = new Container();
+  logo.pivot.x = logo.width / 2;
+  titleScreen.addChild(logo);
+  titleScreen.addChild(instructionText);
 
+  titleScreen.position.set(app.screen.width / 2, app.screen.height / 2);
+
+  const gameScene = new Container();
+  // add element into screen
+  gameScene.addChild(background);
+  gameScene.addChild(board);
+
+  gameScene.position.set(app.screen.width / 2, app.screen.height / 2);
   // Add the bunny to the stage
-  app.stage.addChild(bunny);
+  app.stage.addChild(titleScreen, bunny);
+  // app.stage.addChild(gameScene);
 
-  // Listen for animate update
-  app.ticker.add((time) => {
-    // Just for fun, let's rotate mr rabbit a little.
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
-    bunny.rotation += 0.1 * time.deltaTime;
-  });
+  // // Listen for animate update
+  // app.ticker.add((time) => {
+  //   // Just for fun, let's rotate mr rabbit a little.
+  //   // * Delta is 1 if running at 100% performance *
+  //   // * Creates frame-independent transformation *
+  //   bunny.rotation += 0.1 * time.deltaTime;
+  // });
 })();
---
+++ pixi_Front_End/src/titleScreen.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { Container } from "pixi.js";
+
+export default class TitleScreen extends Container {
+    constructor() {
+        super();
+    }
+}
